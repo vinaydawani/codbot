c23bcc8eee2d67cf37b7a134bc2ea4ad7b19390d
vinaydawani <vdawani6@gmail.com>
2021-05-06 05:20:51 -0400

    logger created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const Discord = require('discord.js');
+const Winston = require('winston');
+const Chalk = require('Chalk');
 const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
+
+const logger = Winston.createLogger({
+	transports: [
+		new Winston.transports.Console({level: 'error'}),
+		new Winston.transports.File({
+			filename: 'src/logs/all.log',
+		}),
+		new Winston.transports.File({
+			filename: 'src/logs/error.log',
+			level: 'error',
+			format: Winston.format.combine(
+				Winston.format.colorize(),
+				Winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
+			),
+		}),
+	],
+	format: Winston.format.printf(log => `[${log.level.toUpperCase()}] - ${log.message}`),
+});
 
 client.once('ready', () => {
 	console.log('Ready!');
